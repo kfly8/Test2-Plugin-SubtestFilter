abc6be3314cddbd682a68a30dc3a699cd240daa4
kobaken <kentafly88@gmail.com>
2025-10-19 23:09:55 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Test2::Plugin::SubtestFilter - It's new $module
+
+# SYNOPSIS
+
+    use Test2::Plugin::SubtestFilter;
+
+# DESCRIPTION
+
+Test2::Plugin::SubtestFilter is ...
+
+# LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kobaken <kentafly88@gmail.com>
---
+++ lib/Test2/Plugin/SubtestFilter.pm
@@ -0,0 +1,39 @@
+package Test2::Plugin::SubtestFilter;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test2::Plugin::SubtestFilter - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test2::Plugin::SubtestFilter;
+
+=head1 DESCRIPTION
+
+Test2::Plugin::SubtestFilter is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
